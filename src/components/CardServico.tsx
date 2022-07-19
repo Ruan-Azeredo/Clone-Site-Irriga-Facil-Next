0f73348f68ae744054cbd4332d9a9600f4c9cf86
Ruan-Azeredo <ruanazeredo@gmail.com>
2022-07-19 14:17:00 -0300

    feat: section serviços

--- src/components/CardServico.tsx
+++ src/components/CardServico.tsx
@@ -1,7 +1,22 @@
-import { EngrenagemIcon } from "./icons/index";
+import Paragrafo from "./Paragrafo";
+import React from "react";
+import { Cog } from "../../node_modules/heroicons-react/build/index";
 
 export default function CardServico() {
     return (
-        <div>{EngrenagemIcon}</div>
+        <div className={`
+            bg-white flex flex-row p-5
+        `}>
+            <div className="flex self-center p-3">
+                <div className={`
+                    text-verdeMenosEscuro font-bold 
+                `}><Cog size={80}/></div>
+            </div>
+            <div className="p-3">
+                <h3 className="text-verdeMenosEscuro font-medium">Titulo do Serviço</h3>
+                <Paragrafo conteudo="Descrição curta do serviço, loren inpsum, loren inpsum, loren inpsum. loren inpsum, loren inpsum, loren. Descrição curta do serviço, loren inpsum, loren inpsum."
+                cor="#443D3D"/>
+            </div>
+        </div>
     )
 }
--- src/components/Servicos.tsx
+++ src/components/Servicos.tsx
@@ -5,13 +5,21 @@ import Titulos from "./Titulos";
 export default function Servicos() {
     return (
         <div className={`
-            bg-azul mt-20 h-screen pt-6
+            bg-azul mt-20 pt-6 pb-1
         `}>
             <Titulos titulo="Serviços" cor="white" corBarra="white" />
             <Paragrafo conteudo="loren inpsum, loren inpsum, loren inpsum." cor="white"
                 className="justify-center mt-4" />
-            
-            <CardServico/>
+            <div className={`
+                grid gap-8 grid-cols-2 m-20
+            `}>
+                <CardServico/>
+                <CardServico />
+                <CardServico />
+                <CardServico />
+                <CardServico />
+                <CardServico/>
+            </div>
         </div>
     )
 }
